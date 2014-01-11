0780cea8890926d489d1c36b7bfc719da5c9fca5
Theo Belaire <theobelaire@khanacademy.org>
2014-01-11 14:38:21 -0800

    Moved the simple counter to it's own file

---
+++ counter.hs
@@ -0,0 +1,25 @@
+
+import Control.Monad (void)
+
+import Reactive.Threepenny
+
+import Graphics.UI.Threepenny.Core
+import qualified Graphics.UI.Threepenny as UI
+
+main :: IO ()
+main = do
+  startGUI defaultConfig setup
+
+timesClicked elem = accumB (0::Int) ((+1) <$ UI.click elem)
+
+setup :: Window -> UI ()
+setup rootWindow = void $ do
+  button <- UI.button #+ [ string "Clickity!" ]
+  outputClicks <- UI.p
+  getBody rootWindow #+
+    map element [ button, outputClicks] --, nameList ]
+
+
+  clicks <- timesClicked button
+  element outputClicks # sink text (show <$> clicks)
+
